e21cb5547c6271fe792d55b4f60e124676caddc1
gastonfeng <gastonfeng@gmail.com>
2023-01-04 01:31:38 +0800

    update zeroconff

--- pythonforandroid/recipes/zeroconf/__init__.py
+++ pythonforandroid/recipes/zeroconf/__init__.py
@@ -3,9 +3,9 @@ from pythonforandroid.recipe import PythonRecipe
 
 class ZeroconfRecipe(PythonRecipe):
     name = 'zeroconf'
-    version = '0.24.5'
+    version = '0.47.1'
     url = 'https://pypi.python.org/packages/source/z/zeroconf/zeroconf-{version}.tar.gz'
-    depends = ['setuptools', 'ifaddr', 'typing;python_version<"3.5"']
+    depends = ['setuptools', 'ifaddr']
     call_hostpython_via_targetpython = False
 
 
